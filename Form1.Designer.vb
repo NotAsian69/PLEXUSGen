18eb64fe3e36c5fcdf56b510dbcac6849e6b164a
NotAsian69 <34027231+NotAsian69@users.noreply.github.com>
2017-11-27 10:59:04 +0000

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class plexusbase
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(plexusbase))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.exitbutton = New System.Windows.Forms.Button()
+        Me.minibtn = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.Font = New System.Drawing.Font("Yu Gothic", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.Black
+        Me.Button1.Location = New System.Drawing.Point(32, 95)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(406, 97)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Generate Plexus"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'exitbutton
+        '
+        Me.exitbutton.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.exitbutton.Font = New System.Drawing.Font("Maiandra GD", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.exitbutton.Location = New System.Drawing.Point(393, 12)
+        Me.exitbutton.Name = "exitbutton"
+        Me.exitbutton.Size = New System.Drawing.Size(36, 30)
+        Me.exitbutton.TabIndex = 6
+        Me.exitbutton.Text = "X"
+        Me.exitbutton.UseVisualStyleBackColor = False
+        '
+        'minibtn
+        '
+        Me.minibtn.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.minibtn.FlatAppearance.BorderSize = 0
+        Me.minibtn.Font = New System.Drawing.Font("Maiandra GD", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.minibtn.Location = New System.Drawing.Point(351, 12)
+        Me.minibtn.Name = "minibtn"
+        Me.minibtn.Size = New System.Drawing.Size(36, 30)
+        Me.minibtn.TabIndex = 7
+        Me.minibtn.Text = "---"
+        Me.minibtn.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Label1.Location = New System.Drawing.Point(176, 210)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(110, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "© LOCALHOST 2017"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.Location = New System.Drawing.Point(219, 52)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(210, 20)
+        Me.TextBox1.TabIndex = 9
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Black
+        Me.Label2.Location = New System.Drawing.Point(29, 52)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(166, 16)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Enter Windows Username:"
+        '
+        'plexusbase
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(460, 261)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.minibtn)
+        Me.Controls.Add(Me.exitbutton)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "plexusbase"
+        Me.Text = "PlexusGen"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents exitbutton As Button
+    Friend WithEvents minibtn As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,60 @@
+﻿Public Class plexusbase
+
+    Dim drag As Boolean
+
+    Dim mousex As Integer
+
+    Dim mousey As Integer
+    Private Sub Form1_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseDown
+
+        drag = True
+
+        mousex = Windows.Forms.Cursor.Position.X - Me.Left
+        mousey = Windows.Forms.Cursor.Position.Y - Me.Top
+
+    End Sub
+
+    Private Sub Form1_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseMove
+
+        If drag Then
+
+            Me.Top = Windows.Forms.Cursor.Position.Y - mousey
+
+            Me.Left = Windows.Forms.Cursor.Position.X - mousex
+
+        End If
+
+    End Sub
+
+    Private Sub Form1_MouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseUp
+
+        drag = False
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+        My.Computer.FileSystem.WriteAllText("C:\PLEXUS\libs\install.bat", My.Computer.FileSystem.ReadAllText("C:\PLEXUS\libs\install.txt").Replace("Unnamed", TextBox1.Text), False)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        System.Diagnostics.Process.Start("C:\PLEXUS\libs\gen.bat") ' hides files
+        System.Diagnostics.Process.Start("C:\PLEXUS\libs\install.bat") ' installs files
+        System.Diagnostics.Process.Start("C:\PLEXUS\Loader.exe") ' runs the hack for the first time
+
+    End Sub
+
+    Private Sub exitbutton_Click(sender As Object, e As EventArgs) Handles exitbutton.Click
+
+        Me.Close()
+
+    End Sub
+
+    Private Sub minibtn_Click(sender As Object, e As EventArgs) Handles minibtn.Click
+
+        Me.WindowState = FormWindowState.Minimized
+
+    End Sub
+End Class
